04ed1f3596ee15d10e26ca3a604a4e7c95c8d0af
glebkz <161125490+glebkz@users.noreply.github.com>
2024-03-30 22:25:06 +0300

    Update first_lab.cpp

--- first_lab.cpp
+++ first_lab.cpp
@@ -7,15 +7,16 @@ struct Part {
     Part* next; //указатель на следующий элемент стека
 };
 
-//шаблон стека
 class STACK {
 public:
     Part* first; //начало стека
-    STACK() : first(nullptr) {}; 
+    STACK() : first(nullptr) {};
+    int size_stack = 0; //создаём переменную с информацией о количестве элементов в стеке
     void in(int v) { //Метод добавления элемента в стек
         Part* part = new Part(); //Создание нового узла
         (*part).info = v; //Присваивание элемента узлу
         (*part).next = first; //Связывание узла с прошлым первым элементом в стеке
+        size_stack++; //увеличиваем размерность стека
         first = part;
     }
     int out() {//Метод извлечения элемента из стека
@@ -23,7 +24,19 @@ public:
         int v = (*first1).info; // Сохранение содержимового по этому указателю
         first = (*first).next; // Второй элемент в стеке теперь первый
         delete(first1); // Освобождение памяти
+        size_stack--; //уменьшаем размерность стека
         return v;
+    }
+    bool isEmpty() { //создаём метод с проверкой стека на пустоту
+        if (size_stack == 0) return true;
+        else return false;
+    }
+    bool isNotEmpty() { //создаём метод с проверкой стека на наличие хотя бы одного элемента
+        if (size_stack == 0) return false;
+        else return true;
+    }
+    int size() { //создаём метод с выводом количества элементов в нём
+        return size_stack;
     }
 };
 
@@ -31,7 +44,7 @@ int main(int argc, char* argv[])
 {
     setlocale(LC_ALL, "Russian"); //Подключаем поддержку русского языка в командной строке
     STACK Stack; //Создаём экземпеляр Stack класса STACK
-    int i = 0, j = 0, x; //Объявляем переменные
+    int x; //Объявляем переменную
     bool argv_after = true;
     if (argv[1] != nullptr) { // проверяем, существует ли аргумент
         std::string s = argv[1];
@@ -44,8 +57,7 @@ int main(int argc, char* argv[])
         {
             std::cin >> x;
             if (x == 0) break;
-            Stack.in(x); 
-            i++;
+            Stack.in(x);
         } while (x != 0);
     }
     else { //запуск с пользовательским интерефейсом
@@ -58,11 +70,10 @@ int main(int argc, char* argv[])
             std::cin >> x; //Вводим число
             if (x == 0) break; //0 не заносится в стек
             Stack.in(x); //Добавляем x в стек
-            i++;
         } while (x != 0);
         std::cout << "Вы ввели 0. Это означает окончание последовательности. Ниже - положительные числа последовательности в обратном порядке:";
     }
-    for (j = i - 1; j >= 0; j--) { //выводим содержимое последовательности в обратном порядке без отрицательных чисел
+    while (Stack.isNotEmpty()) { //выводим содержимое последовательности в обратном порядке без отрицательных чисел, пока в стеке есть числа
         x = Stack.out(); // Сохраняем содержимое 1-ого элемента в стеке в x
         if (x > 0)std::cout << x << std::endl;
     }
